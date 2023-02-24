b5d20ea10c126b3c26292c70489f2e0394b8e72e
Shashwat <kcapshashwat@gmail.com>
2023-02-24 19:21:58 +0530

    Sign up And Sign In Working model

--- app/src/main/java/com/example/khelon/LoginActivity.java
+++ app/src/main/java/com/example/khelon/LoginActivity.java
@@ -1,26 +1,99 @@
 package com.example.khelon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginEmail;
+    EditText loginPassword;
+    Button loginButton;
     TextView switchToSignup;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    final String emailPattern = "^e[1-2][1-9]cseu\\d\\d\\d\\d@bennett.edu.in";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
         switchToSignup = findViewById(R.id.moveto_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        // Move to Sign up Page
         switchToSignup.setOnClickListener(v -> {
-            Intent initiate = new Intent(LoginActivity.this,SignUpActivity.class);
+            Intent initiate = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(initiate);
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginEmailTxt = loginEmail.getText().toString();
+                String loginPasswordTxt = loginPassword.getText().toString();
+
+//                if (loginEmailTxt.isEmpty()) {
+//                    Toast.makeText(LoginActivity.this, "Invalid Email!", Toast.LENGTH_SHORT).show();
+//                    loginEmail.setError("Enter the correct Email!");
+//                } if (loginPasswordTxt.isEmpty() || loginPasswordTxt.length() < 8) {
+//                    Toast.makeText(LoginActivity.this, "Please enter at least 8 Characters of Password!", Toast.LENGTH_SHORT).show();
+//                    loginPassword.setError("Enter a valid password!");
+//                }else {
+//
+//                }
+                if (!loginEmailTxt.isEmpty() && loginEmailTxt.matches(emailPattern)){
+                    if (!loginPasswordTxt.isEmpty()){
+                        mAuth.signInWithEmailAndPassword(loginEmailTxt,loginPasswordTxt)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(LoginActivity.this, "Login In Successful!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(LoginActivity.this, "Email or Password is Wrong!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }else {
+                        loginPassword.setError("Password Cannot be Empty");
+                    }
+                }else if (loginEmailTxt.isEmpty()){
+                    loginEmail.setError("Email cannot be Empty!");
+                }else {
+                    loginEmail.setError("Enter a valid Email!");
+                }
+
+            }
+        });
+
     }
+
     // For back button, not to return back to the previous activity, directly
     // close the application when back button is pressed
     @Override
--- app/src/main/java/com/example/khelon/SignUpActivity.java
+++ app/src/main/java/com/example/khelon/SignUpActivity.java
@@ -1,26 +1,114 @@
 package com.example.khelon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
-    TextView switchToLogin;
+    EditText signupName;
+    EditText signupEmail;
+    EditText signupPassword;
+    EditText signupPhone;
+    Button signupButton;
+    final String emailPattern = "^e[1-2][1-9]cseu\\d\\d\\d\\d@bennett.edu.in";
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        switchToLogin = findViewById(R.id.moveto_login);
+        TextView switchToLogin = findViewById(R.id.moveto_login);
+        signupName = findViewById(R.id.signup_name);
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupPhone = findViewById(R.id.signup_phone);
+        signupButton = findViewById(R.id.signup_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
         switchToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String signupNameTxt = signupName.getText().toString();
+                String signupEmailTxt = signupEmail.getText().toString();
+                String signupPasswordTxt = signupPassword.getText().toString();
+                String signupPhoneNo = signupPhone.getText().toString();
+
+                if (signupNameTxt.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Some of the fields are empty!", Toast.LENGTH_SHORT).show();
+                    signupName.setError("Name Required!");
+                    signupName.requestFocus();
+
+                }
+                if (signupEmailTxt.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Email cannot be Empty!", Toast.LENGTH_SHORT).show();
+                    signupEmail.setError("Email cannot be Empty");
+                    signupEmail.requestFocus();
+
+                }
+                if (!signupEmailTxt.matches(emailPattern)) {
+                    Toast.makeText(SignUpActivity.this, "Invalid Email Format!", Toast.LENGTH_SHORT).show();
+                    signupEmail.setError("Invalid Email Format");
+                    signupEmail.requestFocus();
+
+                }
+                if (signupPasswordTxt.isEmpty() || signupPasswordTxt.length() < 8) {
+                    Toast.makeText(SignUpActivity.this, "Please enter at least 8 Characters of Password!", Toast.LENGTH_SHORT).show();
+                    signupPassword.setError("Enter a valid password!");
+                    signupPassword.requestFocus();
+
+                }
+                if (signupPhoneNo.isEmpty() || signupPhoneNo.length() < 10) {
+                    Toast.makeText(SignUpActivity.this, "Please enter a valid Contact No!", Toast.LENGTH_SHORT).show();
+                    signupPhone.setError("Enter a valid Contact Number");
+                    signupPhone.requestFocus();
+                } else {
+
+                    mAuth.createUserWithEmailAndPassword(signupEmailTxt, signupPasswordTxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SignUpActivity.this, "Sign Up! Successful, Kindly login", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                            } else {
+                                Toast.makeText(SignUpActivity.this, "Sign Up Failed!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
